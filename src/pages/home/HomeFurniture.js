bbda003a850f13c7b5103d56a59484e93bd9fcd0
Abdulrehman Abbasi <abdulrehmanabbasi5067794@gmail.com>
2025-10-12 17:49:18 +0500

    update

--- src/pages/home/HomeFurniture.js
+++ src/pages/home/HomeFurniture.js
@@ -4,7 +4,7 @@ import LayoutOne from "../../layouts/LayoutOne";
 import FeatureIconTwo from "../../wrappers/feature-icon/FeatureIconTwo";
 import LatestProductSection from "../../wrappers/product/LatestProductSection";
 import HeroBanner from "../../components/hero/HeroBanner";
-import TopSlider from "../../components/TopSlider.jsx";
+// import TopSlider from "../../components/TopSlider.jsx";
 import ShopProducts from "../../wrappers/product/ShopProducts";
 import SectionTitle from "../../components/section-title/SectionTitle";
 import ShippingReturnsFeatures from "../../components/features/ShippingReturnsFeatures";
@@ -298,7 +298,7 @@ const HomeFurniture = () => {
         canonical="https://www.alharam.store/"
         robots="index, follow"
       />
-       <TopSlider/>
+       {/* <TopSlider/> */}
       <LayoutOne >
         {/* hero banner section - Instant load */}
        
