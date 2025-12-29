ab472aa4b452d6f8a0071fcd0500419a1b7e8c3d
Abdulrehman Abbasi <abdulrehmanabbasi5067794@gmail.com>
2025-12-29 14:26:18 +0500

    Updated the HeroBanner component to show different images for desktop and mobile.

--- src/components/hero/HeroBanner.js
+++ src/components/hero/HeroBanner.js
@@ -5,12 +5,20 @@
   import SwiperSlider, { SwiperSlide } from "../swiper";
   
   const HeroBanner = ({ spaceBottomClass, spaceTopClass, className }) => {
-    // Banner images from the public folder
-    const bannerImages = [
+    // Desktop banner images from the public folder
+    const desktopBannerImages = [
       "/assets/img/banner/banner1.jpeg",
       "/assets/img/banner/banner2.jpeg",
       "/assets/img/banner/banner3.jpeg",
       "/assets/img/banner/banner4.jpeg",
+    ];
+    
+    // Mobile banner images from the public folder
+    const mobileBannerImages = [
+      "/assets/img/banner/mobile-banner1.png",
+      "/assets/img/banner/mobile-banner2.jpeg",
+      "/assets/img/banner/mobile-banner3.jpeg",
+      "/assets/img/banner/mobile-banner4.jpeg",
     ];
   
     // Swiper options for the banner slider
@@ -43,7 +51,23 @@
             padding: 0;
           }
           
+          .desktop-banner-slider {
+            display: block;
+          }
+          
+          .mobile-banner-slider {
+            display: none;
+          }
+          
           @media (max-width: 768px) {
+            .desktop-banner-slider {
+              display: none !important;
+            }
+            
+            .mobile-banner-slider {
+              display: block !important;
+            }
+            
             .banner-image {
               object-fit: contain !important;
               width: 100% !important;
@@ -118,34 +142,61 @@
           maxWidth: '100%',
           overflow: 'hidden'
         }}>
-          <SwiperSlider options={swiperOptions} navClass="banner-slider-nav">
-            {bannerImages.map((image, index) => (
-              <SwiperSlide key={index}>
-                <div className="single-banner-slide" style={{ 
-                  width: '100%',
-                  margin: 0,
-                  padding: 0,
-                  position: 'relative'
-                }}>
-                  <img 
-                    src={process.env.PUBLIC_URL + image} 
-                    alt={`Banner ${index + 1}`}
-                    style={{
-                      width: '100%',
-                      height: '100%',
-                      objectFit: 'cover',
-                      objectPosition: 'center',
-                      display: 'block',
-                      margin: 0,
-                      padding: 0
-                    }}
-                    className="banner-image"
-                  />
-                </div>
-              </SwiperSlide>
-            ))}
-            <div className="swiper-pagination"></div>
-          </SwiperSlider>
+          {/* Desktop Banner Slider */}
+          <div className="desktop-banner-slider">
+            <SwiperSlider options={swiperOptions} navClass="banner-slider-nav">
+              {desktopBannerImages.map((image, index) => (
+                <SwiperSlide key={`desktop-${index}`}>
+                  <div className="single-banner-slide" style={{ 
+                    width: '100%',
+                    margin: 0,
+                    padding: 0,
+                    position: 'relative'
+                  }}>
+                    <img 
+                      src={process.env.PUBLIC_URL + image} 
+                      alt={`Banner ${index + 1}`}
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                        objectFit: 'cover',
+                        objectPosition: 'center',
+                        display: 'block',
+                        margin: 0,
+                        padding: 0
+                      }}
+                      className="banner-image"
+                    />
+                  </div>
+                </SwiperSlide>
+              ))}
+              <div className="swiper-pagination"></div>
+            </SwiperSlider>
+          </div>
+          
+          {/* Mobile Banner Slider */}
+          <div className="mobile-banner-slider">
+            <SwiperSlider options={swiperOptions} navClass="banner-slider-nav">
+              {mobileBannerImages.map((image, index) => (
+                <SwiperSlide key={`mobile-${index}`}>
+                  <div className="single-banner-slide" style={{ 
+                  
+                    margin: 0,
+                    padding: 0,
+                    position: 'relative'
+                  }}>
+                    <img 
+                      src={process.env.PUBLIC_URL + image} 
+                      alt={`Mobile Banner ${index + 1}`}
+                     
+                      className="banner-image"
+                    />
+                  </div>
+                </SwiperSlide>
+              ))}
+              <div className="swiper-pagination"></div>
+            </SwiperSlider>
+          </div>
         </div>
       </div>
       </>
